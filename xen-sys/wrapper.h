f8e966a83475eadbb67417c46e577778e3330f25
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-02-24 15:30:44 -0700

    xen-sys: Don't hard code architecture types
    Header file include/xen/types.h has all the type definition needed for
    and architecture.  It is smart enough to reference the right asm/types.h
    based on the target architecture.
    
    As such remove the hard coded types in favour or including xen/types.h.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- xen-sys/wrapper.h
+++ xen-sys/wrapper.h
@@ -1,11 +1,4 @@
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+#include <xen/types.h>
 #include <public/xen.h>
 #include <public/sched.h>
 #include <public/elfnote.h>
