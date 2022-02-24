64eb1456299ba85282aed08c057c7d9025b82aca
Mathieu Poirier <mathieu.poirier@linaro.org>
2022-02-24 15:40:42 -0700

    xen-sys: Add mechanic to properly generate bindings for aarch64
    Bindgen defaults to the x86 architecture, regardless of the value of
    ARCH on the command line.  As such add the proper mechanic to identify
    other architecture and set environment flags in a way that bindings
    for the aarch64 can be generated properly.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>

--- xen-sys/wrapper.h
+++ xen-sys/wrapper.h
@@ -1,3 +1,11 @@
+
+#if ARCH == aarch64
+#undef __386__
+#undef __x86_64__
+#define __aarch64__
+#define CONFIG_ARM_64
+#endif
+
 #include <xen/types.h>
 #include <public/xen.h>
 #include <public/sched.h>
